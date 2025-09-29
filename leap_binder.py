7d7c7f535c8fe07395c1f08a9f999a14a1ecefac
Tom Koren <tom.koren@tensorleap.ai>
2025-09-29 17:58:41 +0300

    add code mapping and decorators

--- leap_binder.py
+++ leap_binder.py
@@ -1,6 +1,9 @@
 from typing import List, Dict, Union
 import numpy as np
 import numpy.typing as npt
+from code_loader.inner_leap_binder.leapbinder_decorators import tensorleap_preprocess, tensorleap_unlabeled_preprocess, \
+    tensorleap_input_encoder, tensorleap_gt_encoder, tensorleap_metadata, tensorleap_custom_visualizer, \
+    tensorleap_custom_loss
 from keras.datasets import cifar10
 
 # Tensorleap imports
@@ -13,26 +16,36 @@ from cifar10_resnet.utils import metadata_animal, metadata_fly, metadata_label_n
 from cifar10_resnet.data.preprocess import preprocess_func
 from cifar10_resnet.encoders import input_encoder
 from cifar10_resnet.config import CONFIG
+from code_loader.contract.enums import DataStateType
+from keras.losses import CategoricalCrossentropy
 
 
 # Preprocess Function
+@tensorleap_preprocess()
 def preprocess_func_leap() -> List[PreprocessResponse]:
     train_X, val_X, train_Y, val_Y =preprocess_func()
 
     # Generate a PreprocessResponse for each data slice, to later be read by the encoders.
     # The length of each data slice is provided, along with the data dictionary.
-    train = PreprocessResponse(length=2000, data={'images': train_X, 'labels': train_Y, 'subset_name': 'train'})
-    val = PreprocessResponse(length=1000, data={'images': val_X, 'labels': val_Y, 'subset_name': 'val'})
-    response = [train, val]
+    train = PreprocessResponse(length=2000, data={'images': train_X, 'labels': train_Y, 'subset_name': 'train'},
+                               state=DataStateType.training)
+    val = PreprocessResponse(length=1000, data={'images': val_X, 'labels': val_Y, 'subset_name': 'val'},
+                             state=DataStateType.validation)
+
+    _, (test_X, _) = cifar10.load_data()
+    unlabeled =  PreprocessResponse(length=1000, data={'images': test_X, 'subset_name': 'unlabeled'},
+                                    state=DataStateType.unlabeled)
+
+    response = [train, val, unlabeled]
     return response
 
-
-def unlabeled_data() -> PreprocessResponse:
-    _, (test_X, _) = cifar10.load_data()
-    return PreprocessResponse(length=1000, data={'images': test_X, 'subset_name': 'unlabeled'})
+@tensorleap_custom_loss('ce')
+def ce_loss(gt, y_pred):
+    return CategoricalCrossentropy(reduction='none')(gt, y_pred).numpy()
 
 # Input encoder fetches the image with the index `idx` from the `images` array set in
 # the PreprocessResponse data. Returns a numpy array containing the sample's image.
+@tensorleap_input_encoder('image')
 def input_encoder_leap(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
     # return preprocess.data['images'][idx].astype('float32')
     image = preprocess.data['images'][idx].astype('float32')
@@ -40,12 +53,15 @@ def input_encoder_leap(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
 
 # Ground truth encoder fetches the label with the index `idx` from the `labels` array set in
 # the PreprocessResponse's data. Returns a numpy array containing a hot vector label correlated with the sample.
+@tensorleap_gt_encoder('classes')
 def gt_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
         return preprocess.data['labels'][idx].astype('float32')
 
+@tensorleap_metadata('sample_index')
 def metadata_sample_index(idx: int, preprocess: PreprocessResponse) -> int:
     return idx
 
+@tensorleap_metadata('metadata')
 def metadata_dict(idx: int, preprocess: PreprocessResponse) -> Dict[str, Union[float, int, str]]:
     if preprocess.data['subset_name'] == 'unlabeled':
         gt_label = -1
@@ -69,7 +85,7 @@ def metadata_dict(idx: int, preprocess: PreprocessResponse) -> Dict[str, Union[f
     }
     return res
 
-
+@tensorleap_custom_visualizer('horizontal_bar_lm', LeapDataType.HorizontalBar)
 def horizontal_bar_visualizer_with_labels_name(data: npt.NDArray[np.float32]) -> LeapHorizontalBar:
     data = np.squeeze(data)
     labels_names = [CONFIG['LABELS_NAMES'][index] for index in range(data.shape[-1])]
@@ -77,14 +93,14 @@ def horizontal_bar_visualizer_with_labels_name(data: npt.NDArray[np.float32]) ->
 
 
 # Dataset binding functions to bind the functions above to the `Dataset Instance`.
-leap_binder.set_preprocess(function=preprocess_func_leap)
-leap_binder.set_unlabeled_data_preprocess(function=unlabeled_data)
-leap_binder.set_input(function=input_encoder_leap, name='image')
-leap_binder.set_ground_truth(function=gt_encoder, name='classes')
-leap_binder.set_metadata(function=metadata_sample_index, name='sample_index')
-leap_binder.set_metadata(function=metadata_dict, name='metadata')
-leap_binder.set_visualizer(horizontal_bar_visualizer_with_labels_name, 'horizontal_bar_lm', LeapDataType.HorizontalBar)
-leap_binder.add_prediction(name='classes', labels=CONFIG['LABELS_NAMES'])
+# leap_binder.set_preprocess(function=preprocess_func_leap)
+# leap_binder.set_unlabeled_data_preprocess(function=unlabeled_data)
+# leap_binder.set_input(function=input_encoder_leap, name='image')
+#leap_binder.set_ground_truth(function=gt_encoder, name='classes')
+#leap_binder.set_metadata(function=metadata_sample_index, name='sample_index')
+#leap_binder.set_metadata(function=metadata_dict, name='metadata')
+#leap_binder.set_visualizer(horizontal_bar_visualizer_with_labels_name, 'horizontal_bar_lm', LeapDataType.HorizontalBar)
+#leap_binder.add_prediction(name='classes', labels=CONFIG['LABELS_NAMES'])
 
-if __name__ == '__main__':
-    leap_binder.check()
+# if __name__ == '__main__':
+#     leap_binder.check()
---
+++ leap_integration.py
@@ -0,0 +1,62 @@
+import os
+
+import tensorflow as tf
+import numpy as np
+from os.path import exists
+import urllib
+from keras.losses import CategoricalCrossentropy
+
+from leap_binder import (preprocess_func_leap, input_encoder_leap, gt_encoder, metadata_dict, metadata_sample_index
+, horizontal_bar_visualizer_with_labels_name, ce_loss)
+from leap_binder import leap_binder
+from code_loader.plot_functions.visualize import visualize
+from code_loader.contract.datasetclasses import PredictionTypeHandler
+from code_loader.inner_leap_binder.leapbinder_decorators import tensorleap_load_model, integration_test
+from cifar10_resnet.config import CONFIG
+from code_loader.contract.enums import DataStateType
+
+prediction_type1 = PredictionTypeHandler('classes', CONFIG['LABELS_NAMES'])
+
+@tensorleap_load_model([prediction_type1])
+def load_model():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    model_path = 'model/resnet.h5'
+    cnn = tf.keras.models.load_model(os.path.join(dir_path, model_path))
+    return cnn
+
+@integration_test()
+def check_custom_intgeration(idx, subset):
+    plot_vis = True
+    inpt = input_encoder_leap(idx, subset)
+    resnet = load_model()
+    y_pred = resnet(inpt)
+    sample_index = metadata_sample_index(idx, subset)
+    dict_metadata = metadata_dict(idx, subset)
+
+    horizontal_bar_pred = horizontal_bar_visualizer_with_labels_name(y_pred)
+
+    if plot_vis:
+        visualize(horizontal_bar_pred, 'Prediction')
+
+    #if subset and subset.state != DataStateType.unlabeled:
+    gt = gt_encoder(idx, subset)
+    ls = ce_loss(gt, y_pred)
+
+    horizontal_bar_gt = horizontal_bar_visualizer_with_labels_name(gt)
+
+    if plot_vis:
+        visualize(horizontal_bar_gt, 'GT')
+
+
+def check_custom_integration():
+
+    responses = preprocess_func_leap()
+
+    check_custom_intgeration(0, responses[0])
+    print("Custom tests finished successfully")
+
+if __name__ == '__main__':
+    check_custom_integration()
+
+
+
