225210016de8a764043af647b89afaef16682776
Idan <idan@idan.com>
2026-02-17 08:50:19 +0200

    >

--- leap_integration.py
+++ leap_integration.py
@@ -1,13 +1,15 @@
 import os
 import tensorflow as tf
 from leap_binder import (preprocess_func_leap, input_encoder_leap, gt_encoder, metadata_dict, metadata_sample_index
-, horizontal_bar_visualizer_with_labels_name, ce_loss)
+, horizontal_bar_visualizer_with_labels_name, ce_loss, image_visualizer)
 from code_loader.plot_functions.visualize import visualize
 from code_loader.contract.datasetclasses import PredictionTypeHandler
 from code_loader.inner_leap_binder.leapbinder_decorators import tensorleap_load_model, tensorleap_integration_test
 from cifar10_resnet.config import CONFIG
 
 prediction_type1 = PredictionTypeHandler('classes', CONFIG['LABELS_NAMES'])
+
+
 
 @tensorleap_load_model([prediction_type1])
 def load_model():
@@ -20,6 +22,8 @@ def load_model():
 def check_custom_intgeration(idx, subset):
     plot_vis = True
     inpt = input_encoder_leap(idx, subset)
+
+    img_vis = image_visualizer(inpt)
     resnet = load_model()
     y_pred = resnet(inpt)
     sample_index = metadata_sample_index(idx, subset)
